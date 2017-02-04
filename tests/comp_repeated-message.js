bdb0e975dbc52df83fb9f34cc4d6348e900bd2d7
Aakil Fernandes <aakilfernandes@gmail.com>
2017-02-03 20:00:17 -0500

    repeated empty array

--- tests/comp_repeated-message.js
+++ tests/comp_repeated-message.js
@@ -13,8 +13,17 @@ var msg = { inner: [{}, {}, {}] };
 tape.test("repeated messages", function(test) {
     var root = protobuf.parse(proto).root,
         Outer = root.lookup("Outer");
-    
+
     var dec = Outer.decode(Outer.encode(msg).finish());
     test.same(dec, msg, "should encode and decode back to the original values");
+    test.end();
+});
+
+tape.test("empty repeated messages", function(test) {
+    var root = protobuf.parse(proto).root,
+        Outer = root.lookup("Outer");
+
+    var dec = Outer.decode(Outer.encode({}).finish());
+    test.same(dec, { inner: [] }, "should decode with empty array");
     test.end();
 });
